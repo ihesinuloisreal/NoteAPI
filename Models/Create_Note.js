ecf21177918402d9b4d2c784f22fc8c9e57e4ff4
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-05-05 09:00:29 -0700

    model

--- Models/Create_Note.js
+++ Models/Create_Note.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
-const NoteSchema = Schema({
-    Note{
-        type: String,
-    };
+const NoteSchema = new Schema({
+    id: {type: Number},
 });
