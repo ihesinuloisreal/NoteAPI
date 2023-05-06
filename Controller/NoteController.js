92cbd6c658adc34e228c965ebe56195022e0c9e5
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-05-06 15:52:28 -0700

    mysql

--- Controller/NoteController.js
+++ Controller/NoteController.js
@@ -1,5 +1,24 @@
+const { createPool} = require("mysql");
+
+const pool = createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "Note",
+    connectionLimit: 10
+})
+
+
+
 module.exports = {
     get: (req, res) => {
-        res.send("Welcome to this Router")
+        pool.query('select * from note', (err, result, fields)=> {
+            if(err){
+                return console.log(err);
+            }
+            // return console.log()
+            res.send("name = " +result[1].name)
+
+        })
     }
 }
