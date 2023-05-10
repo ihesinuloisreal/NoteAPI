a284a8d9cfb06c72f9c91a473c29570c292f29b7
Ihesinulo Isreal <ihesinuloisreali@gmail.com>
2023-05-10 12:50:25 -0700

    connected

--- Controller/NoteController.js
+++ Controller/NoteController.js
@@ -1,41 +1,39 @@
 const { createPool} = require("mysql");
 
-const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "Note",
-    connectionLimit: 10
-})
+// const pool = createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "Note",
+//     connectionLimit: 10
+// })
 
-let sql = `INSERT INTO note (title, text, color, date) VALUE ?;`;
 
 
 
 module.exports = {
     
     get: (req, res, next) => {
-        pool.query('select name from note', (err, result, fields)=> {
-            if(err){
-                res.send(err);
-            }
-            // return console.log()
-            res.send("name = " +result[1].name)
+        // pool.query('select name from note', (err, result, fields)=> {
+        //     if(err){
+        //         res.send(err);
+        //     }
+        //     // return console.log()
+        //     res.send("name = " +result[1].name)
 
-        })
+        // })
     },
     
     post: (req, res, next) => {
-        console.log(req.body.data)
-        let {values} = req.body
+        // console.log(req.body.data)
 
-        pool.query(sql,values, (err, result, fields)=> {
-            if(err){
-                res.send(err);
-            }
-            // return console.log("")
-            res.send('Successful')
+        // pool.query("INSERT INTO note (title, text, color, date) VALUE (title, )", (err, result, fields)=> {
+        //     if(err){
+        //         res.send(err);
+        //     }
+        //     // return console.log("")
+        //     res.send('Successful')
 
-        })
+        // })
     }
 }
